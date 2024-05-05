4d8ee304b98ab9c449476a3f6ca21ad3c4414a8d
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-05 14:29:39 -0300

    feat: add correct dependencie to icons

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { HomeIcon } from "@primer/octicons-react";
 import Link from "next/link";
 import React from "react";
 
@@ -5,6 +6,7 @@ export const NavBar = () => {
 	return (
 		<nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2'>
 			<Link className='mr-2' href='/'>
+				<HomeIcon size={24} />
 				<span>Home</span>
 			</Link>
 			<div className='flex flex-1'></div>
