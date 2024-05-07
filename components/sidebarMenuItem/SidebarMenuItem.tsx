3b1b7e5f5197a2730688c60f45aede240559fa19
Narela Camara <50678398+RoxanaCamara@users.noreply.github.com>
2024-05-07 16:16:46 +0000

    feat: icons and components

---
+++ components/sidebarMenuItem/SidebarMenuItem.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+export interface Props {
+  path: string;
+  icon: JSX.Element;
+  title: string;
+  subTitle: string;
+}
+
+export default function SidebarMenuItem({
+  path,
+  title,
+  subTitle,
+  icon,
+}: Props) {
+  return (
+    <a
+      href={path}
+      className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150"
+    >
+      <div>{icon}</div>
+      <div className="flex flex-col">
+        <span className="text-lg font-bold leading-5 text-white">{title}</span>
+        <span className="text-sm text-white/50 hidden md:block">
+          {subTitle}
+        </span>
+      </div>
+    </a>
+  );
+}
