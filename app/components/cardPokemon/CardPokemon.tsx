d391235a0202559ce6eabff2cf652ca2f17c6590
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-11 00:25:15 -0300

    feat: pririty false

--- app/components/cardPokemon/CardPokemon.tsx
+++ app/components/cardPokemon/CardPokemon.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
+import { IoHeartOutline } from "react-icons/io5";
 
 export interface ICardPokemonProps {
 	name: string;
@@ -21,10 +22,11 @@ export function CardPokemon({ name, id, url }: ICardPokemonProps) {
 						className='h-24 w-24 text-white rounded-full mx-auto'
 						width='32'
 						height='32'
+						priority={false}
 					/>
 
 					<p className='pt-2 text-lg font-semibold text-gray-50'>{name}</p>
-					
+
 					<div className='mt-5'>
 						<a className='border rounded-full py-2 px-4 text-xs font-semibold text-gray-100'>
 							Manage your Account
@@ -33,47 +35,15 @@ export function CardPokemon({ name, id, url }: ICardPokemonProps) {
 				</div>
 				<div className='border-b'>
 					<Link
-						href='/account/campaigns'
-						className='px-4 py-2 hover:bg-gray-100 flex'>
-						<div className='text-green-600'>
-							<svg
-								fill='none'
-								stroke='currentColor'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='1'
-								viewBox='0 0 24 24'
-								className='w-5 h-5'>
-								<path d='M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z' />
-							</svg>
+						href={`/pokemon/${id}`}
+						className='px-4 py-2 hover:bg-gray-100 flex justify-center'>
+						<div className='text-red-600'>
+							<IoHeartOutline />
 						</div>
 						<div className='pl-3'>
 							<p className='text-sm font-medium text-gray-800 leading-none'>
-								Campaigns
+								Favorito
 							</p>
-							<p className='text-xs text-gray-500'>View your campaigns</p>
-						</div>
-					</Link>
-					<Link
-						href='/account/donations'
-						className='px-4 py-2 hover:bg-gray-100 flex'>
-						<div className='text-gray-800'>
-							<svg
-								fill='none'
-								stroke='currentColor'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='1'
-								viewBox='0 0 24 24'
-								className='w-5 h-5'>
-								<path d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z' />
-							</svg>
-						</div>
-						<div className='pl-3'>
-							<p className='text-sm font-medium text-gray-800 leading-none'>
-								Donations
-							</p>
-							<p className='text-xs text-gray-500'>View your last donations</p>
 						</div>
 					</Link>
 				</div>
