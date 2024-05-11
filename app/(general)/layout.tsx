c110c62135aa8f77ca69c09b5af5bafc9a9ec1ae
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-10 23:30:48 -0300

    feat: correction

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -6,9 +6,11 @@ export default function AboutLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<>
-			<Sidebar />
-			{children}
-		</>
+		<div className='bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white'>
+			<div className='flex'>
+				<Sidebar />
+				<div className='w-full text-slate-900'>{children}</div>
+			</div>
+		</div>
 	);
 }
