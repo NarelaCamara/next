8f7d3726754fdd83f0abb9a92450d3e2b8eb608f
RoxanaCamara <roxana.camara07@gmail.com>
2024-05-12 14:12:03 -0300

    feat: correction in name function

--- app/(general)/pokemon/[id]/page.tsx
+++ app/(general)/pokemon/[id]/page.tsx
@@ -1,14 +1,13 @@
 import { URL_API } from "@/app/helpers/constants";
 import { Metadata } from "next";
-import React from "react";
 
 interface Props {
 	params: { id: string };
 }
 
-export async function generateMetada({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
 	const pokemon = await getPokemon(params.id);
-	console.log(pokemon);
+
 	return {
 		title: `#${params.id} -${pokemon.name}`,
 		description: `Pokemon ${pokemon.name}`,
@@ -26,7 +25,6 @@ const getPokemon = async (id: string) => {
 
 export default async function PokemonPage({ params }: Props) {
 	const pokemon = await getPokemon(params.id);
-	console.log(pokemon);
 
 	return (
 		<div>
