39b54afaeb7361f99afdfae836da99b79dcedf3a
RoxanaCamara <roxana.camara07@gmail.com>
2024-06-19 15:00:48 -0300

    feat: add monserrat style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { Montserrat } from "next/font/google";
 import Link from "next/link";
+
+const monserrat = Montserrat({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Arial", "sans-serif"],
+});
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <span className="text-5xl">Hola Nareko!</span>
-        <Link href="/main">Main page</Link>
-      </main>
-   
+    <main className={monserrat.className}>
+      <span className="text-5xl">Hola Nareko!</span>
+      <Link href="/main">Main page</Link>
+    </main>
   );
 }
