7dd164831bc92c145a0ca4f83cf49d3a8ff594b4
RoxanaCamara <roxana.camara07@gmail.com>
2024-06-22 17:19:39 -0300

    feat: card

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -9,9 +9,11 @@ interface Props {
 
 export const Card = ({ image }: Props) => {
   return (
-    <div className=" w-1/3 h-2/3 m-4 bg-white p-6 mb-6 shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer border">
-      <div className="p-6">
-        <span className="text-2xl font-bold my-4">Portafolio</span>
+    <div className=" w-1/3 h-2/3 m-4 bg-white p-6 mb-6 shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl  cursor-pointer border">
+      <div className="p-6 flex flex-col">
+        <p className="justify-start mt-4 text-base font-bold">2020/01</p>
+        <span className="text-2xl font-bold mt-0 mb-8">Semi Senior Frontend</span>
+        <span className="text-2xl font-bold">Accenture</span>
         {image && <Image alt="User_photo" className=" w-2/3" src={image} />}
         <Paragraph
           paragraph="Consequuntur nesciunt.
