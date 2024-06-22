cbe8952b9b7f8959ccb06172427eda8d1d7d4d2d
RoxanaCamara <roxana.camara07@gmail.com>
2024-06-22 16:51:51 -0300

    feat: carrousel

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import { Card } from "../card/Card";
 import {
@@ -14,16 +13,14 @@ interface Props {
 
 export const Carousel = ({ image }: Props) => {
   return (
-    <section className="p-4 w-10/12">
-      <div className="w-full flex flex-row-reverse flex-nowrap bg-white m-8 ">
-        <div className="flex flex-row flex-nowrap">
-          <DotFillIcon size={16} className="opacity-60" />
-          <DotFillIcon size={16} className="opacity-60" />
-          <DotIcon size={16} className="opacity-60" />
-        </div>
+    <section className="p-8 w-full">
+      <div className="w-2/2 flex flex-row flex-nowrap bg-white m-8 justify-end">
+        <DotFillIcon size={16} className="opacity-60" />
+        <DotFillIcon size={16} className="opacity-60" />
+        <DotIcon size={16} className="opacity-60" />
       </div>
 
-      <div className=" flex items-center">
+      <div className=" w-full flex items-center">
         <button className="middle none center h-12 min-h-[48px] w-12 min-w-[48px] rounded-full bg-slate-600 font-sans text-sm font-bold uppercase text-white shadow-md bg-slate-600/20 transition-all hover:shadow-lg hover:bg-slate-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
           <ChevronLeftIcon size={24} />
         </button>
